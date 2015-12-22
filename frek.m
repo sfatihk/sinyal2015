2931da13b8753ca177c581fb12cd05d72e30f959
Seyyid Fatih KOÇ <sakkarmen@gmail.com>
2015-12-22 11:38:29 +0200

    hafta3 guncellemesi
    notalar güncellendi, sus olcusu eklendi, varsayilan oktav 4 yapildi,

--- frek.m
+++ frek.m
@@ -1,12 +1,23 @@
 function [ nota_frek ] = frek( nota, oktav )
-notalar={'do','c#','re','eb','mi','fa','f#','sol','g#','la','bb','si'}; %notalari tanimladim
+notalar={'Do','Dod','Re','Mib','Mi','Fa','Fad','Sol','Sold','La','Sib','Si','sus'}; %notalari tanimladim
+nota_frek=0; %frekans sus gelme ihtimaline karsi sifirlaniyor
 
-nota_index=find(ismember(notalar,nota)); %arguman olarak gelen nota,
-                                         %eger notalar dizisinin elemaniysa
-                                         %bu elemanin indisini cekiyorum
-                                         %boylece nota indexi olusuyor
+if nargin < 2  % eger oktav belirtilmemisse,
+    oktav = 4; % 4.oktav kabul ediliyor.
+end
 
-f0 = 440; %La4(nota#10,oktav#4) notasini pivot olarak belirledim
-nota_frek=f0*2^((nota_index-10)/12 + (oktav-4)); %la4 notasi 10.nota ve 4.oktav oldugu icin
-                                                 %fonksiyona bu farklari dahil ettim
+nota_index=find(ismember(notalar,nota)); 
+%arguman olarak gelen nota; eger notalar dizisinin elemaniysa,
+%bu elemanin indisini cekiyorum. Boylece nota indexi(degeri) olusuyor.
+
+
+if nota_index ~= 13 %eger 13.nota(sus) geldiyse frekans hesaplanmiyor.                                        
+    
+    f0 = 440; %La4(nota#10,oktav#4) notasini pivot olarak belirledim
+    
+    nota_frek=f0*2^((nota_index-10)/12 + (oktav-4)); 
+    %notalar arasinda 2^(n/12)lik,oktavlar arasında 2^n lik oran vardir.
+    %la4 notasi 10.nota ve 4.oktav oldugu icin fonksiyona bu farklari dahil ettim
+end
+
 end
