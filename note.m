bbcedb701bf1ec72bbc7af28de0f7d1c31340f54
Seyyid Fatih KOÇ <sakkarmen@gmail.com>
2015-12-29 12:10:32 +0200

    hafta4guncellemesi
    ses sinyaline 2. , 3. ve 4. harmonikler belirli oranlarda eklendi

--- note.m
+++ note.m
@@ -9,8 +9,14 @@ function [x,t] = note(frekans,sure)
     linspace(1.5,1,Fs*sure/8),...      % T/8'lik surede [1.5 - 1] azalis
     linspace(1,1,4*Fs*sure/8),...      % 4T/8'lik surede [1]sabit
     linspace(1,0,Fs*sure/8+1)];        % T/8'lik surede [1 - 0] azalis
-
-    x=A.*sin(2*pi*frekans*t);
+    
+    h0=sin(2*pi*frekans*t); %orijinal
+    h1=sin(2*pi*frekans*t*2); %harmonik1
+    h2=sin(2*pi*frekans*t*3); %harmonik2
+    h3=sin(2*pi*frekans*t*4); %harmonik3
+    h=h0+0.8*h1+0.4*h2+0.1*h3; % %80.h1 + %40.h2 + %10.h3
+    
+    x=A.*h;
     %katsayilar vektoru, sinus sinyali ile carpilarak,
     %zarfa uygun bir cikis sinyali olusturuluyor.
     %Not: .* kullaniminin sebebi matlabin matris carpimi hatasi vermesini
