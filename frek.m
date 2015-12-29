d143e7dc5f703ceda392668543180ac9194972ac
Seyyid Fatih KOÇ <sakkarmen@gmail.com>
2015-12-29 12:08:52 +0200

    hafta4-fonksiyonel degisiklik yok

--- frek.m
+++ frek.m
@@ -12,9 +12,7 @@ nota_index=find(ismember(notalar,nota));
 
 
 if nota_index ~= 13 %eger 13.nota(sus) geldiyse frekans hesaplanmiyor.                                        
-    
     f0 = 440; %La4(nota#10,oktav#4) notasini pivot olarak belirledim
-    
     nota_frek=f0*2^((nota_index-10)/12 + (oktav-4)); 
     %notalar arasinda 2^(n/12)lik,oktavlar arasında 2^n lik oran vardir.
     %la4 notasi 10.nota ve 4.oktav oldugu icin fonksiyona bu farklari dahil ettim
